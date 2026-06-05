e9569a53e3d2f1765c72301d00dd2672b3364a52
monik1422 <monikkumar2204@gmail.com>
2026-06-05 19:45:18 -0400

    dynamodb state locking

--- day05/main.tf
+++ day05/main.tf
@@ -4,4 +4,10 @@ resource "aws_vpc" "main" {
     Name = "tf-vpc-day05"
   }
 }
-
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.128/26"
+  tags = {
+    Name = "tf-vpc-day05"
+  }
+}
