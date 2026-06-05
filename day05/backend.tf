c55fd79c2a8fc26bbd64aeda5021e0f7ca6fdf9c
monik1422 <monikkumar2204@gmail.com>
2026-06-05 19:36:30 -0400

    dynamodb_table = "terraform-lock-table"

--- day05/backend.tf
+++ day05/backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
     bucket = "tf-bkt-rmt-state-bknd002"
     key    = "terraform.tfstate"
-    use_lockfile = true 
-    region = "us-east-1"
+    #use_lockfile = true
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
   }
 }
--- day05/main.tf
+++ day05/main.tf
@@ -4,3 +4,4 @@ resource "aws_vpc" "main" {
     Name = "tf-vpc-day05"
   }
 }
+
